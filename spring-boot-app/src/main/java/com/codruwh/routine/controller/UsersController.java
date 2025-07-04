38fc7be034347a42603962d39c9317b57621e7a2
김민준 <owtdxv@gmail.com>
2025-07-04 18:23:46 +0900

    fix: add swagger description

--- spring-boot-app/src/main/java/com/codruwh/routine/controller/UsersController.java
+++ spring-boot-app/src/main/java/com/codruwh/routine/controller/UsersController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,8 @@ import com.codruwh.routine.application.UsersService;
 import com.codruwh.routine.controller.dto.ProfileResponseDto;
 import com.codruwh.routine.domain.Profile;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 /**
@@ -20,16 +23,20 @@ import lombok.RequiredArgsConstructor;
  */
 @RestController
 @RequestMapping("/api/users")
+@Tag(name = "사용자 관련 API")
 @RequiredArgsConstructor
 public class UsersController {
 
   private final UsersService usersService;
 
+  @Operation(
+    summary = "사용자 정보 조회",
+    description = "현재 로그인한 사용자의 프로필 정보를 조회합니다. AccessToken이 필요합니다"
+  )
   @GetMapping("/me")
-  public ResponseEntity<ProfileResponseDto> getMyProfile(@AuthenticationPrincipal UserDetails userDetails) {
+  public ResponseEntity<ProfileResponseDto> getMyProfile(@RequestHeader("Authorization") String authorizationHeader, @AuthenticationPrincipal UserDetails userDetails) {
     // JwtAuthenticationFilter에서 UserDetails의 username에 UID가 들어갑니다
     UUID userId = UUID.fromString(userDetails.getUsername());
-    System.out.println(userDetails);
     Profile profile = usersService.getProfileByUserId(userId);
 
     return ResponseEntity.ok(new ProfileResponseDto(profile));
